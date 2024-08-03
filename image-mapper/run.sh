b1e3bdc98fcf3341466ebc58d4eea2bbe24fa6d3
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-03 14:48:45 +0200

    Add run.sh wrapper

---
+++ image-mapper/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+die () {
+    echo "$1"
+    exit 1
+}
+
+src="/src"
+dst="/dst"
+
+if [[ ! -d "$src" ]]; then
+    die "src does not exist"
+
+elif [[ -z "$( ls -A $src )" ]]; then
+    die "src is empty"
+
+elif [[ ! -d "$dst" ]]; then
+    die "dst does not exist"
+
+elif [[ -z "${QUALITY}" ]]; then
+    die "QUALITY env not set"
+
+elif [[ -z "${VIDEOS}" ]]; then
+    die "VIDEOS env not set"
+
+else
+    if [[ "$VIDEOS" = "true" ]]; then
+        videos="--include-videos"
+    else
+        videos=""
+    fi
+    
+    exec "image_mapper" "$src" "$dst" "$QUALITY" "$videos" "--verbose"
+fi
+
