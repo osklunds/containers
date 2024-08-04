856640ad34167781d6f4de92a2888f9b76b354f4
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-04 08:56:03 +0200

    Add compose file for http-file-server

--- http-file-server/run.sh
+++ http-file-server/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -d "/files" ]]; then
-    exec "/usr/app/node_modules/http-server/bin/http-server" "/files" "-c-1"
+    exec "/usr/app/node_modules/http-server/bin/http-server" "/files" "-c-1" "-p 80"
 else
     echo "/files doesn't exist"
     exit 1
